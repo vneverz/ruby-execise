ebedfcb64612fd483cb7a76bf6c2139098277216
vneverz <vneverz@gmail.com>
2015-03-24 15:59:25 +0800

    done basic ex3.rb

---
+++ basic/ex3.rb
@@ -0,0 +1,14 @@
+movies = {
+
+  UndertheSkin: 2014,
+  Manofsteel: 2013,
+  I_Am_Love: 2010,
+  Rabbit_without_ears: 2007,
+  Blood_Diamond: 2006
+}
+
+puts movies[:UndertheSkin]
+puts movies[:I_Am_Love]
+puts movies[:Manofsteel]
+puts movies[:Rabbit_without_ears]
+puts movies[:Blood_Diamond]
