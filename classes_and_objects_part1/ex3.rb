dbeaed25a7e3e52a6f8b05c73e3b9f538acba2bf
vneverz <vneverz@gmail.com>
2015-03-25 09:01:42 +0800

    done Class-part1 ex2 & ex3

---
+++ classes_and_objects_part1/ex3.rb
@@ -0,0 +1,43 @@
+class MyCar
+
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color
+  end
+
+end
+
+Toyota = MyCar.new(1999, 'chevy Toyota', 'white')
+Toyota.color = 'blue'
+puts Toyota.color
+puts Toyota.year
+Toyota.spray_paint('red')
+puts Toyota.color
