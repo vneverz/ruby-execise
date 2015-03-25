83bca73311003b3609e8ef945791e6e665459212
vneverz <vneverz@gmail.com>
2015-03-25 18:43:27 +0800

    done classes_part2 ex1,ex2

---
+++ classes_and_objects_part2/ex1.rb
@@ -0,0 +1,42 @@
+class MyCar
+
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color
+  end
+
+end
+
+MyCar.gas_mileage(16, 351)
